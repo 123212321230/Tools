063b96c9285816a038a45af578afcce3c1b480c5
周慧敏 <84626690+123212321230@users.noreply.github.com>
2022-11-03 10:11:50 +0800

    Rename 图片下载.py to img_download.py

---
+++ img_download.py
@@ -0,0 +1,5 @@
+from urllib import request
+#info:sslvpn.swjtu.edu.cn
+#username:2016044855
+#passwd:swjtu2022
+request.urlretrieve('https://img2020.cnblogs.com/blog/35695/202110/35695-20211008160624813-1694591598.jpg','1.jpg')
